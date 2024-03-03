3da1c9e281953aba051402b76e7248d0bf439b55
Anastasia Hvorostova <hvorostok8@gmail.com>
2024-03-04 00:33:42 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Chess.com API
+*********************************
+This project is a ***REST web-service*** uses [Chess.com API](https://www.chess.com/news/view/published-data-api).
---
+++ src/main/java/hvorostina/chesscomapi/ChessComApiApplication.java
@@ -0,0 +1,13 @@
+package hvorostina.chesscomapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessComApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessComApiApplication.class, args);
+    }
+
+}
