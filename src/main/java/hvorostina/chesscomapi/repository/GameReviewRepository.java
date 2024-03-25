48389bfe50a97847a17bb0c76d378417d3d36b1d
Anastasia Hvorostova <hvorostok8@gmail.com>
2024-03-25 12:00:42 +0300

    style: remove unused imports

--- src/main/java/hvorostina/chesscomapi/repository/GameReviewRepository.java
+++ src/main/java/hvorostina/chesscomapi/repository/GameReviewRepository.java
@@ -3,10 +3,8 @@ package hvorostina.chesscomapi.repository;
 import hvorostina.chesscomapi.model.GameReview;
 import hvorostina.chesscomapi.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface GameReviewRepository extends JpaRepository<GameReview, Integer> {
     List<GameReview> findAllByUser(Player user);
